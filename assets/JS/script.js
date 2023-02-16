92fe05a8f1337083f1b6691f7865f4d866884398
zzltxla <ata@edenschool.fr>
2023-02-16 16:27:01 +0100

    fifth commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,11 @@
 const tetris = document.getElementById("tetris");
-const context = tetris.getContext('2d');
+const ctx = tetris.getContext('2d');
 
-context.scale(10, 10);
+ctx.scale(10, 10);
 
 
 // All pieces created
-const pieces  = [
+const pieces = [
     [
         [1, 1],
         [1, 1]
@@ -42,26 +42,95 @@ const pieces  = [
 ];
 
 const player = {
-    pos: {x: 0, y: 0},
-    matrix: generatePiece(),
+    pos: { x: 5, y: 0 },
+    matrix: generatePiece(),    //genrates random pieces 
 }
 
-function generatePiece () {     //randomizing a piece
-    let random = Math.floor(Math.random() * pieces.length); 
-    return pieces[random];  
+function generatePiece() {     //randomizing a piece
+    let random = Math.floor(Math.random() * pieces.length);
+    return pieces[random];
 }
 
-function drawMatrix(matrix, x, y){
-    for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[i].length; j++){
-            if(matrix[i][j]){
-                context.fillRect(x + j, y + i, 1, 1);
+function drawMatrix(matrix, x, y) {      //creates the piece on the document
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j]) {
+                ctx.fillRect(x + j, y + i, 1, 1);
             }
         }
     }
 }
 
+let interval = 1000;
+let lastTime = 0;
+let count = 0;
+function downpieces(time = 0){
+    const dt = time - lastTime;
+    lastTime = time;
+    count += dt;
+    if(count >= interval){
+        player.pos.y++;
+        count = 0;
+    }
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, tetris.width, tetris.height);
+    ctx.fillStyle = "red";
+    drawMatrix(player.matrix, player.pos.x, player.pos.y);
+    requestAnimationFrame(downpieces);
+}
+downpieces();
+
+function moveLeft() {
+    ctx.beginPath();
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, tetris.width, tetris.height);
+    ctx.fillStyle = "red";
+    drawMatrix(player.matrix, player.pos.x--, player.pos.y);
+    ctx.endPath();
+    requestAnimationFrame(moveLeft);
+}
+
+function moveRight() {
+    ctx.beginPath();
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, tetris.width, tetris.height);
+    ctx.fillStyle = "red";
+    drawMatrix(player.matrix, player.pos.x++, player.pos.y);
+    ctx.endPath();
+    requestAnimationFrame(moveRight);
+}
+
+
 drawMatrix(player.matrix, player.pos.x, player.pos.y);
+
+
+
+function getKey(e) {
+    let keyCode = e.key;
+    console.log(keyCode);
+}
+
+window.addEventListener('keydown', function (event) {
+    let key = event.key;
+    switch (key) {
+        case 'ArrowLeft':
+            moveLeft(player.pos.x);
+            break;
+        case 'ArrowRight':
+            moveRight(player.pos.x);
+            break;
+    }
+    // case 38 : 
+    //     changeOrientation(); 
+    //     break;
+    // case keyCode == 'ArrowRight': 
+
+    //     break;
+    // case 40 : 
+    //     moveDown();
+    //     break;
+}
+);
 
 
 const background = document.querySelector('#background');
@@ -71,13 +140,3 @@ const ctxBackground = background.getContext("2d");
 ctxBackground.scale(20, 20);
 ctxBackground.fillStyle = 'red';
 ctxBackground.fillRect(0, 0, 10, 20);
-
-
-
-
-
-
-
-
-
-//generation on pieces region ends here 
