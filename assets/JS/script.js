c84e6d405ed6dd474234d58809c15cb5329e2cd4
zzltxla <ata@edenschool.fr>
2023-03-01 15:29:11 +0100

    twelth commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,7 +5,7 @@ ctx.scale(20, 20);
 
 
 // All pieces 
-const pieces = [
+let pieces = [
     [
         [1, 1],
         [1, 1]
@@ -50,6 +50,7 @@ const player = {
     speed : 1,
     width : 0,
     height : 0,
+    interactivityEnabled : true,
 }
 
 function generatePiece() {     //randomizing a piece
@@ -100,9 +101,12 @@ function drawMatrix(matrix, x, y) {      //creates the piece on the document
             if (matrix[i][j]) {
                 ctx.fillRect(x + j, y + i, 1, 1);
             }
+            
         }
     }
 }
+
+drawMatrix(player.matrix, player.pos.x, player.pos.y);
 
 let interval = 1000;
 let lastTime = 0;
@@ -129,7 +133,6 @@ function moveLeft() {   //when left arrow pressed, moves towards the left
     while (player.pos.x + player.width !== 0 + player.width) {
         ctx.beginPath();
         player.pos.x -= player.speed;
-        ctx.fillStyle = "#000";
         ctx.fillRect(0, 0, tetris.width, tetris.height);
         drawMatrix(player.matrix, player.pos.x, player.pos.y);
         ctx.endPath();
@@ -157,26 +160,132 @@ function moveDown () {
 
 function toBottom() {
     let lastPos = player.pos.y = 19 - player.height + 1;
-    return lastPos;
+    requestAnimationFrame(toBottom);
+    if (lastPos) {
+        tetris.save();
+    }
+    drawMatrix(player.matrix, player.pos.x, player.pos.y);
 }
 
-drawMatrix(player.matrix, player.pos.x, player.pos.y);
+function changeOrientation() {
+    ctx.rotate(90 );
+}
 
-// switch (player.pos.y) {
-//     case player.pos.y = 19 - player.height + 1 : 
-//     while (this.disabled(true)) {
-//         drawMatrix(player.matrix, player.pos.x, player.pos.y);
+// function changeOrientation() {
+//     switch (player.matrix) {
+//         case pieces[1] :
+//             pieces[1] = [
+//                 [1, 1, 1, 1],
+//             ];
+//             ctx.fillStyle = "#43e8ca"; //turquoise I block
+//             ctx.fillRect(0, 0, tetris.width, tetris.height);
+//             player.width = 4;
+//             player.height = 1;
+//             break;
+//         case pieces[2] : 
+//             pieces[2] = [
+//                 [0, 1],
+//                 [1, 1],
+//                 [1, 0]
+//             ];
+//             ctx.fillStyle = "#06d618";  //green z block
+//             ctx.fillRect(0, 0, tetris.width, tetris.height);
+//             player.width = 2;
+//             player.height = 3;
+//             break;
+//         case pieces[3] : 
+//             pieces[3] = [
+//                 [1, 0], 
+//                 [1, 1], 
+//                 [0, 1],
+//             ];
+//             ctx.fillStyle = "red"; //red s block
+//             ctx.fillRect(0, 0, tetris.width, tetris.height);
+//             player.width = 2;
+//             player.height = 3;
+//             break;
+//         case pieces[4] : 
+//             pieces[4] = [
+//                 [
+//                     [0, 0, 1],
+//                     [1, 1, 1],
+//                 ], 
+//                 [
+//                     [1, 1],
+//                     [0, 1],
+//                     [0, 1],
+//                 ],
+//             ];
+//             ctx.fillStyle = "#f75c02"; //orange L block
+//             for (let b = 0; b <= pieces[4].length; b++) {
+//                 if (b == 0) {
+//                     player.width = 3;
+//                     player.height = 2;
+//                 } else {
+//                     player.width = 2;
+//                     player.height = 3;
+//                 }
+//             }
+//             ctx.fillRect(0, 0, tetris.width, tetris.height);
+//             break;
+//         case pieces[5] :
+//             pieces[5] = [
+//                 [
+//                     [1, 0, 0],
+//                     [1, 1, 1],
+//                 ],
+//                 [
+//                     [1, 1],
+//                     [1, 0],
+//                     [1, 0],
+//                 ],
+//             ];
+//             ctx.fillStyle = "#080ec7"; //night blue J block
+//             for (let a = 0; a <= pieces[5].length; a++) {
+//                 if (a == 0) {
+//                     player.width = 3;
+//                     player.height = 2;
+//                 } else {
+//                     player.width = 2;
+//                     player.height = 3;
+//                 }
+//             };
+//             ctx.fillRect(0, 0, tetris.width, tetris.height);
+//             break;
+//         case pieces[6] : 
+//             pieces[6] = [
+//                 [
+//                     [0, 1, 0],
+//                     [1, 1, 1],
+//                 ],
+//                 [
+//                     [1, 0],
+//                     [1, 1],
+//                     [1, 0],
+//                 ],
+//                 [
+//                     [0, 1],
+//                     [1, 1],
+//                     [0, 1],
+//                 ],
+//             ];
+//             ctx.fillStyle = "#980ee3";  //purple T block
+//             for (let i = 0; i <= pieces[6].length; i++) {
+//                 if (i == 0) {
+//                     player.width = 3;
+//                     player.height = 2;
+//                 } else {
+//                     player.width = 2;
+//                     player.height = 3;
+//                 };
+//             }
+//             ctx.fillRect(0, 0, tetris.width, tetris.height);
+//             break;
 //     }
-//     break;
+//     requestAnimationFrame(changeOrientation);
 // }
 
-// async function newPiece() {
-//     await lastPos; 
-//     this.disabled(true);
-//     drawMatrix(player.matrix, player.pos.x, player.pos.y);
-//     return player;
-// }
-
+let movements =
 window.addEventListener('keydown', function (event) {
     let key = event.key;
     switch (key) {
@@ -192,11 +301,10 @@ window.addEventListener('keydown', function (event) {
         case ' ' : 
             toBottom()
             break;
+        case 'ArrowUp' : 
+            changeOrientation(); 
+            break; 
     }
-    // case 38 : 
-    //     changeOrientation(); 
-    //     break;
-    // case keyCode == 'ArrowRight': 
 });
 
 
